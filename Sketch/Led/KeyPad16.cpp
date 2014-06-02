54f656c678d26bfae6b26ba8a0583043b72b5eb7
Herbert <Herbert@29acaefb-a8ad-b042-8ced-20973511b138>
2014-06-02 21:31:25 +0000

    git-svn-id: svn://ait1/Arduino/Src@44 29acaefb-a8ad-b042-8ced-20973511b138

---
+++ Sketch/Led/KeyPad16.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include "KeyPad16.h"
+
+
+void CKeyPad16::Init()
+{
+  pinMode(PAD_IN + 0, INPUT_PULLUP);
+  pinMode(PAD_IN + 1, INPUT_PULLUP);
+  pinMode(PAD_IN + 2, INPUT_PULLUP);
+  pinMode(PAD_IN + 3, INPUT_PULLUP);
+
+  pinMode(PAD_OUT + 0, INPUT);
+  pinMode(PAD_OUT + 1, INPUT);
+  pinMode(PAD_OUT + 2, INPUT);
+  pinMode(PAD_OUT + 3, INPUT);
+}
+
+bool CKeyPad16::GetKey(unsigned char outpin, unsigned char inpin)
+{
+  digitalWrite(outpin, LOW);
+  pinMode(outpin, OUTPUT);
+  unsigned char ret = (digitalRead(inpin) == LOW) ? true : false;
+  digitalWrite(outpin, HIGH);
+  pinMode(outpin, INPUT);
+  return ret;
+}
+
+bool CKeyPad16::GetKey(unsigned char key)
+{
+  unsigned char row = key / 4;
+  unsigned char col = 3 - (key % 4);
+
+  return GetKey(PAD_OUT + row, PAD_IN + col);
+}
+
+unsigned int CKeyPad16::GetAllKeys()
+{
+  unsigned int keys = 0;
+  for (unsigned char key = 0 ; key < 16; key++)
+  {
+    if (GetKey(key))
+    {
+      keys += (1 << key);
+    }
+  }
+
+  return keys;
+}
+
--- Sketch/Led/Led.ino
+++ Sketch/Led/Led.ino
@@ -13,29 +13,92 @@
 #include "Wheel.h"
 #include "Box.h"
 
-COLOR Display[NUM_LEDS];  
+#include "KeyPad16.h"
 
-void setup() 
+
+COLOR Display[NUM_LEDS];
+
+CKeyPad16 keypad;
+
+void setup()
 {
   Serial.begin(9600);
   randomSeed(analogRead(0));
-  
+
+  keypad.Init();
+
   BL.gridX = 15;
   BL.gridY = 10;
-  BL.Init(NUM_LEDS,Display);
+  BL.Init(NUM_LEDS, Display);
 }
 
-void loop() 
+void KeyPadLoop(unsigned long maxtime);
+
+void loop()
 {
-	BoxLoop(10000);
-	WheelLoop(10000);
-	BallLoop(100000,true);
-	ClockLoop(9,false,1000);
-        TickerLoop("Herbert Aitenbichler",ScrollFromRight,1000);
-	ClockLoop(5,false,2000);
-	TickerLoop("He",ScrollSingleCharFadeInRightFadeOutRight,1000);
-	TickerLoop("rb",ScrollSingleCharFadeInRightFadeOutLeft,1000);
-	TickerLoop("er",ScrollSingleCharFadeInRightFadeOutUp,1000);
-	TickerLoop("t",ScrollSingleCharFadeInRightFadeOutDown,1000);
-	ClockLoop(25,true,250);
+  TickerLoop("Press Key", ScrollFromRight, 1000);
+  KeyPadLoop(10000);
+  //return;
+  BoxLoop(10000);
+  WheelLoop(10000);
+  BallLoop(100000, true);
+  ClockLoop(9, false, 1000);
+  TickerLoop("Herbert Aitenbichler", ScrollFromRight, 1000);
+  ClockLoop(5, false, 2000);
+  TickerLoop("He", ScrollSingleCharFadeInRightFadeOutRight, 1000);
+  TickerLoop("rb", ScrollSingleCharFadeInRightFadeOutLeft, 1000);
+  TickerLoop("er", ScrollSingleCharFadeInRightFadeOutUp, 1000);
+  TickerLoop("t", ScrollSingleCharFadeInRightFadeOutDown, 1000);
+  ClockLoop(25, true, 250);
 }
+
+void KeyPadLoop(unsigned long maxtime)
+{
+  unsigned int Counter, Counter2, Counter3;
+  Counter = 0;
+
+  unsigned long exittime = millis() + maxtime;
+
+  BL.SetAll(BL.Color(0, 0, 0));
+
+  while (millis() < exittime)
+  {
+    unsigned int keys = keypad.GetAllKeys();
+    static unsigned int oldkeys = 0;
+
+    if (keys != oldkeys)
+    {
+
+      BL.SetAll(BL.Color(0, 0, 0));
+
+      oldkeys = keys;
+
+      bool have = false;
+      for (unsigned char key = 0 ; key < 16; key++)
+      {
+        if ((keys % 2) == 1)
+        {
+          if (have)
+          {
+//            Serial.print(F("-"));
+          }
+          else
+          {
+            have = true;
+          }
+          BL.SetPixel(key, BL.Color(31, 31, 31));
+//          Serial.print((unsigned char )key + 1);
+        }
+        keys /= 2;
+      }
+/* 
+      if (have)
+        Serial.println();
+      else
+        Serial.println(F("no"));
+*/        
+    }
+    BL.Show();
+  }
+}
+
