aaf8dec35f3cff6e89436fd8b6f4847933fcd605
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2014-06-25 23:47:10 +0200

    GNU General Public License

--- VC/GCode/GCode.GUI/PaintForm.cs
+++ VC/GCode/GCode.GUI/PaintForm.cs
@@ -1,4 +1,22 @@
-﻿using Framework.Tools;
+﻿////////////////////////////////////////////////////////
+/*
+  This file is part of CNCLib - A library for stepper motors.
+
+  Copyright (c) 2013-2014 Herbert Aitenbichler
+
+  CNCLib is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  CNCLib is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  http://www.gnu.org/licenses/
+*/
+
+using Framework.Tools;
 using GCode.Logic.Load;
 using GCode.Logic.Commands;
 using GCode.Logic;
