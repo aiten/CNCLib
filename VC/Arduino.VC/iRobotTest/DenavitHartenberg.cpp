b7c6fb3c20560606b414f7ffa8b271cc9abaa01a
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2015-07-16 23:28:56 +0200

    DH Inverse

--- VC/Arduino.VC/iRobotTest/DenavitHartenberg.cpp
+++ VC/Arduino.VC/iRobotTest/DenavitHartenberg.cpp
@@ -69,6 +69,81 @@ void CDenavitHartenberg::ToPosition(float in[NUM_AXIS], float out[3])
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
+void CDenavitHartenberg::FromPosition(float posxyz[3], float angles[NUM_AXIS])
+{
+
+	for (unsigned char i = 0; i < 100; i++)
+	{
+		for (unsigned char j = 0; j < 3; j++)
+		{
+			if (SearchMin(posxyz, angles, j, 0, M_PI / 2 + M_PI/4) < 0.01)
+				return;
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+float CDenavitHartenberg::SearchMin(float pos[3], float inout[NUM_AXIS], unsigned char idx, float min, float max)
+{
+	if (max < min)
+	{
+		float tmp = min;
+		min = max;
+		max = tmp;
+	}
+
+	if (inout[idx] > max)
+		inout[idx] = max;
+
+	if (inout[idx] < min)
+		inout[idx] = min;
+
+	inout[idx] = min;
+
+	float dist = (max-min) / 10; 
+	float oldiff = CalcDist(pos, inout);
+	float diff = oldiff;
+
+	while (inout[idx] >= min && inout[idx] <= max)
+	{
+		inout[idx] += dist;
+		float diff = CalcDist(pos, inout);
+
+		//printf("%f:%f:%f => %f\n", inout[0], inout[1], inout[2], diff);
+
+		if (diff < 0.001) break;
+
+		if (diff > oldiff)
+		{
+			dist = -dist / 2;
+			if (fabs(dist) < 0.0000001)
+				break;
+		}
+		oldiff = diff;
+	}
+	return diff;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+float CDenavitHartenberg::CalcDist(float pos[3], float in[NUM_AXIS])
+{
+	float v[4] = { 0, 0, 0, 1 };
+	CMatrix4x4<float> A;
+
+	InitMatrix(A, in);
+	TestConvert(A, v,true);
+
+	float dist = 0;
+	for (unsigned char i = 0; i < 3; i++)
+		dist += (pos[i] - v[i]) * (pos[i] - v[i]);
+
+	return dist;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #define FORMAT_MM "%.0f:%.0f:%.0f"
 #define FORMAT_GRAD "%.0f:%.0f:%.0f"
 
@@ -81,5 +156,5 @@ void CDenavitHartenberg::TestConvert(CMatrix4x4<float>&m, float inout[4], bool o
 	if (out)
 		memcpy(inout, d, sizeof(d));
 
-	printf(FORMAT_MM"\n", d[0], d[1], d[2]);
+	//printf(FORMAT_MM"\n", d[0], d[1], d[2]);
 }
--- VC/Arduino.VC/iRobotTest/DenavitHartenberg.h
+++ VC/Arduino.VC/iRobotTest/DenavitHartenberg.h
@@ -10,6 +10,7 @@ public:
 
 
 	void ToPosition(float in[NUM_AXIS], float out[3]);
+	void FromPosition(float in[3], float out[NUM_AXIS]);
 
 protected:
 
@@ -19,6 +20,10 @@ private:
 
 
 	void TestConvert(CMatrix4x4<float>&m, float inout[4], bool out=false);
+
+	float SearchMin(float pos[3], float inout[NUM_AXIS], unsigned char idx, float min, float max);
+
+	float CalcDist(float pos[3], float in[NUM_AXIS]);
 
 };
 
