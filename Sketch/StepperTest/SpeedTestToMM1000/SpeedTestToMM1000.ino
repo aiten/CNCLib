f7afa98b22ef9b33803a009fb4b573ec95c83c6b
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2015-12-13 11:04:21 +0100

    Performance Test

--- Sketch/StepperTest/SpeedTestToMM1000/SpeedTestToMM1000.ino
+++ Sketch/StepperTest/SpeedTestToMM1000/SpeedTestToMM1000.ino
@@ -2,7 +2,7 @@ typedef unsigned char axis_t;  // type for "axis"
 typedef long mm1000_t;
 typedef long sdist_t;
 
-typedef mm1000_t(*ToMm1000_t) (axis_t axis, sdist_t val);
+//typedef mm1000_t(*ToMm1000_t) (axis_t axis, sdist_t val);
 typedef sdist_t(*ToMachine_t) (axis_t axis, mm1000_t val);
 
 void setup() 
@@ -32,24 +32,10 @@ inline long RoundMulDivI32(long v, long m, long d)
   return (v * m + d / 2) / d;
 }
 
-inline long ToMm1000_float(axis_t /* axis */, long val)               { return  (long) (val * (80.0/256.0)); }
-inline long  ToMachine_float(axis_t /* axis */, long val)             { return  (long) (val * (256.0/80.0)); }
-
-inline long ToMm1000_float_round(axis_t /* axis */, long val)         { return  (long) ((val * (80.0/256.0))+0.5); }
-inline long  ToMachine_float_round(axis_t /* axis */, long val)       { return  (long) ((val * (256.0/80.0))+0.5); }
-
-inline mm1000_t ToMm1000_1_3200(axis_t /* axis */, sdist_t val)        { return  MulDivU32(val, 80, 256); }
-inline sdist_t  ToMachine_1_3200(axis_t /* axis */, mm1000_t val)      { return  MulDivU32(val, 256, 80); }
-inline mm1000_t ToMm1000_1_3200R(axis_t /* axis */, sdist_t val)       { return  RoundMulDivU32(val, 80, 256); }
-  
-
-inline mm1000_t ToMm1000Inch_1_3200(axis_t /* axis */, sdist_t val)    { return  MulDivU32(val, 25, 2032); }
-inline sdist_t  ToMachineInch_1_3200(axis_t /* axis */, mm1000_t val)  { return  MulDivU32(val, 2032, 25); }
-
-
-void TestFnc(ToMm1000_t fnc, char* text) 
+void TestFnc(ToMachine_t fnc, const char* text) 
 {
-  unsigned long timeto = millis()+10000;
+  unsigned long timetogo=5;
+  unsigned long timeto = millis()+(timetogo*1000);
   unsigned long i=0;
 
   while (millis()< timeto)
@@ -69,15 +55,20 @@ void TestFnc(ToMm1000_t fnc, char* text)
 
   Serial.print(text);
   Serial.print(i);
+  Serial.print(F("\t= "));
+  Serial.print(float(i)/timetogo);
   Serial.println();
 }
 
 void loop() 
 {
-  TestFnc(ToMm1000_float, "float: ");
-  TestFnc(ToMm1000_float_round, "floatR:");
-  TestFnc(ToMm1000_1_3200,"int1:  ");
-  TestFnc(ToMm1000_1_3200R,"int1r: ");
-  TestFnc(ToMm1000Inch_1_3200,"int2:  ");
+  TestFnc( [] (axis_t /* axis */, mm1000_t val) { return  (sdist_t) (val * (256.0/80.0)); },          "float: ");
+ // TestFnc( [] (axis_t /* axis */, mm1000_t val) { return  (sdist_t) ((val * (256.0/80.0))+0.5); },    "floatR:");
+  TestFnc( [] (axis_t /* axis */, mm1000_t val) { return  (sdist_t) MulDivI32(val, 256, 80); },              "int1:  ");
+// TestFnc( [] (axis_t /* axis */, mm1000_t val) { return  (sdist_t) MulDivI32(val, 16,  5); },              "int1B: ");
+  TestFnc( [] (axis_t /* axis */, mm1000_t val) { return  (sdist_t) MulDivU32(val, 17,  16); },              "int1B: ");
+  TestFnc( [] (axis_t /* axis */, mm1000_t val) { return  (sdist_t) MulDivU32(val, 80, 256); },              "int1A: ");
+//  TestFnc( [] (axis_t /* axis */, mm1000_t val) { return  (sdist_t) RoundMulDivU32(val, 256, 80); },         "int1r: ");
+//  TestFnc( [] (axis_t /* axis */, mm1000_t val) { return  (sdist_t) MulDivU32(val, 2032, 25); },             "int2:  ");
 }
 
