558eae8dca4d5adb9ef7182518c2a20e00a050b0
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2016-04-03 21:21:07 +0200

    Missing files

---
+++ VC/CNCLib/CNCLib.Wpf/Helpers/JoystickArduinoSerialCommunication.cs
@@ -0,0 +1,42 @@
+﻿using CNCLib.Wpf.ViewModels.ManualControl;
+using Framework.Arduino;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CNCLib.Wpf.Helpers
+{
+    class JoystickArduinoSerialCommunication : ArduinoSerialCommunication
+    {
+        private Framework.Arduino.ArduinoSerialCommunication Com
+        {
+            get { return Framework.Tools.Pattern.Singleton<Framework.Arduino.ArduinoSerialCommunication>.Instance; }
+        }
+
+        public void AsyncRunCommand(Action todo)
+        {
+            new Thread(() =>
+            {
+                try
+                {
+                    todo();
+                    Com.WriteCommandHistory(CommandHistoryViewModel.CommandHistoryFile);
+                }
+                finally
+                {
+                }
+            }
+            ).Start();
+        }
+
+        protected override void OnReplyReceived(ArduinoSerialCommunicationEventArgs info)
+        {
+            base.OnReplyReceived(info);
+
+            AsyncRunCommand(() => Com.SendCommand(info.Info));
+        }
+    }
+}
