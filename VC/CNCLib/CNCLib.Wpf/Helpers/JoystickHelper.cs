0de01f4167c978bd03957024b629c3104fec0f96
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2016-05-04 18:18:38 +0200

    Alignment edit controls

---
+++ VC/CNCLib/CNCLib.Wpf/Helpers/JoystickHelper.cs
@@ -0,0 +1,60 @@
+﻿////////////////////////////////////////////////////////
+/*
+  This file is part of CNCLib - A library for stepper motors.
+
+  Copyright (c) 2013-2016 Herbert Aitenbichler
+
+  CNCLib is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  CNCLib is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  http://www.gnu.org/licenses/
+*/
+
+using System.Linq;
+using CNCLib.Wpf.Models;
+using Framework.Tools.Dependency;
+using CNCLib.Logic.Contracts;
+
+namespace CNCLib.Wpf.Helpers
+{
+	public class JoystickHelper
+	{
+		internal static Joystick Load(out int id)
+		{
+			using (var controller = Dependency.Resolve<IItemController>())
+			{
+				var joystick = controller.GetAll(typeof(Models.Joystick));
+				if (joystick != null && joystick.Count() > 0)
+				{
+					id = joystick.First().ItemID;
+					return (Models.Joystick)controller.Create(id);
+				}
+			}
+			id = -1;
+
+			return new Joystick() { BaudRate = 250000, ComPort = @"com7" };
+
+		}
+
+		internal static void Save(Joystick joystick, ref int id)
+		{
+			using (var controller = Dependency.Resolve<IItemController>())
+			{
+				if (id >= 0)
+				{
+					controller.Save(id, "Joystick", joystick);
+				}
+				else
+				{
+					id = controller.Add("Joystick", joystick);
+				}
+			}
+		}
+	}
+}
--- VC/CNCLib/CNCLib.Wpf/Views/ManualControlPage.xaml.cs
+++ VC/CNCLib/CNCLib.Wpf/Views/ManualControlPage.xaml.cs
@@ -17,29 +17,17 @@
 */
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using CNCLib.Wpf.ViewModels;
 using Framework.Wpf.ViewModels;
 
 namespace CNCLib.Wpf
 {
-    /// <summary>
-    /// Interaction logic for ManualControl.xaml
-    /// </summary>
-    public partial class ManualControlPage : Page
+	/// <summary>
+	/// Interaction logic for ManualControl.xaml
+	/// </summary>
+	public partial class ManualControlPage : Page
     {
         public ManualControlPage()
         {
