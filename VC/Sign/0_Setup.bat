5cd90e82d9d406cacd2fd44244b1098608c5a89b
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2016-12-16 07:54:18 +0100

    Change Cert

--- VC/Sign/0_Setup.bat
+++ VC/Sign/0_Setup.bat
@@ -1,7 +1,7 @@
 set SRCDRV=%~p0
 set KeyName=CNCLib
 rem set SIGNToolPath="C:\Program Files\Microsoft SDKs\Windows\v7.1A\bin\"
-rem set SIGNToolPath="C:\Program Files (x86)\Windows Kits\8.1\bin\x86\"
-set SIGNToolPath="C:\Program Files (x86)\Windows Kits\10\bin\x86\"
+set SIGNToolPath="C:\Program Files (x86)\Windows Kits\8.1\bin\x86\"
+rem set SIGNToolPath="C:\Program Files (x86)\Windows Kits\10\bin\x86\"
 set SIGNTool=%SIGNToolPath%signtool.exe
 pause
--- VC/Sign/1_CreateZertifikat.bat
+++ VC/Sign/1_CreateZertifikat.bat
@@ -1,6 +1,6 @@
-﻿call 0_Setup.bat
+call 0_Setup.bat
 
-%SIGNToolPath%makecert -a SHA256 -sv %SRCDRV%\%KeyName%.pvk -n "CN=DI Herbert Aitenbichler - Josef-Holzmann-Weg 1 - 4060 Leonding - Austria" %SRCDRV%\%KeyName%.cer -r
+%SIGNToolPath%makecert -a SHA256 -sv %SRCDRV%\%KeyName%.pvk -n "CN=DI Herbert Aitenbichler" %SRCDRV%\%KeyName%.cer -r
 %SIGNToolPath%pvk2pfx -pvk %SRCDRV%\%KeyName%.pvk -spc %SRCDRV%\%KeyName%.cer -pfx %SRCDRV%\%KeyName%.pfx -po "%1"
 
 pause
