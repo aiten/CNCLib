a640c473b93e6602dc9ef88621b06d73e882a2be
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2017-11-10 14:19:32 +0100

    Test

--- VS/Sign/3_DelaySignException.cmd
+++ VS/Sign/3_DelaySignException.cmd
@@ -1,11 +1,15 @@
 rem sn -k CNCLib.snk
-sn -Vr *,f1dd8891e96f0824
-sn -Vl
 
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vr *,f1dd8891e96f0824
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vl
+rem sn -Vr *,f1dd8891e96f0824
+rem sn -Vl
+
+rem "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vr *,f1dd8891e96f0824
+rem "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vl
 
 "\\ait7\c$\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vr *,f1dd8891e96f0824
 "\\ait7\c$\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vl
+
+"\\ait7\c$\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\sn.exe" -Vr *,f1dd8891e96f0824
+"\\ait7\c$\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\sn.exe" -Vl
 
 pause
