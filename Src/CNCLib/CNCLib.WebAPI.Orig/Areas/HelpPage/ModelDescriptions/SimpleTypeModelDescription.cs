906b2d52ef425141bd55774abe2536fc4a896f2a
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2018-02-01 16:13:02 +0100

    WebAPI.Orig => remove

--- Src/CNCLib/CNCLib.WebAPI.Orig/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CNCLib.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Src/CNCLib/CNCLib.WebAPI.Orig/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CNCLib.WebAPI.WebApiApplication" Language="C#" %>
