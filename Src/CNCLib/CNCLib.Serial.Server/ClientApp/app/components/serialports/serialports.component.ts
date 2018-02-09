6edbacb43c362989c56b8e5fed6a8e9f7042dacb
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2018-02-09 10:13:08 +0100

    Copyright header

--- Src/CNCLib/CNCLib.Serial.Server/ClientApp/app/components/serialports/serialports.component.ts
+++ Src/CNCLib/CNCLib.Serial.Server/ClientApp/app/components/serialports/serialports.component.ts
@@ -1,6 +1,26 @@
+////////////////////////////////////////////////////////
+/*
+  This file is part of CNCLib - A library for stepper motors.
+
+  Copyright (c) 2013-2018 Herbert Aitenbichler
+
+  CNCLib is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  CNCLib is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  http://www.gnu.org/licenses/
+*/
+
 import { Component, Inject, OnInit } from '@angular/core';
 import { SerialPortDefinition } from '../../models/serial.port.definition';
 import { SerialServerService } from '../../services/serialserver.service';
+import { Router } from '@angular/router';
+import { machinecontrolURL } from '../machinecontrol/machinecontrol.routing';
 
 @Component({
     selector: 'serialports',
@@ -12,7 +32,8 @@ export class SerialPortsComponent
     public historyportid!: number;
 
     constructor(
-        private serivalServerService: SerialServerService
+        private serivalServerService: SerialServerService,
+        private router: Router
     )
     {
     }
@@ -35,6 +56,11 @@ export class SerialPortsComponent
     showHistory(showhistoryportid: number)
     {
         this.historyportid = showhistoryportid;
+    }
+
+    machineControl(showhistoryportid: number)
+    {
+        this.router.navigate([machinecontrolURL, showhistoryportid]);
     }
 
     async clearQueue(serialportid: number): Promise<void>
