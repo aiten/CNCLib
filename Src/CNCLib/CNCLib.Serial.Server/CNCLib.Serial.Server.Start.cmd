92e727c73b1dfe53d171c8298da475c8ce0af4a9
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2018-03-05 20:39:25 +0100

    Start Serial Server

--- Src/CNCLib/CNCLib.Serial.Server/CNCLib.Serial.Server.Start.cmd
+++ Src/CNCLib/CNCLib.Serial.Server/CNCLib.Serial.Server.Start.cmd
@@ -1,1 +1,2 @@
-dotnet "%~dp0\CNCLib.Serial.Server.dll" --urls "http://*:5000"
+start "CNCLib.Serial.Server" dotnet "%~dp0\CNCLib.Serial.Server.dll" --urls "http://*:5000"
+start "CNCLib.Serial.Server Web-Interface"  http://localhost:5000
