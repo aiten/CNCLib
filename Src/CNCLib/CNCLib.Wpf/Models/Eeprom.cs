5b66d98da68b598ad15e5415625c51ec796a0a0e
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2019-01-11 09:30:46 +0100

    (c) 2019

--- Src/CNCLib/CNCLib.Wpf/Models/Eeprom.cs
+++ Src/CNCLib/CNCLib.Wpf/Models/Eeprom.cs
@@ -2,7 +2,7 @@
 /*
   This file is part of CNCLib - A library for stepper motors.
 
-  Copyright (c) 2013-2014 Herbert Aitenbichler
+  Copyright (c) 2013-2019 Herbert Aitenbichler
 
   CNCLib is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
