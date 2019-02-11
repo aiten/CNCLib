068cbad0270748fa3cef0e6d19762123250b21f6
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2019-02-11 19:43:43 +0100

    UriPath

---
+++ Src/Framework/Framework.Tools/Uri/UriPathExtensions.cs
@@ -0,0 +1,42 @@
+﻿/*
+  This file is part of CNCLib - A library for stepper motors.
+
+  Copyright (c) 2013-2019 Herbert Aitenbichler
+
+  CNCLib is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  CNCLib is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  http://www.gnu.org/licenses/
+*/
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Web;
+
+namespace Framework.Tools.Uri
+{
+    public static class UriPathExtensions
+    {
+        public static string ToUriAsQuery(this object val)
+        {
+            if (val is DateTime dt)
+            {
+                if (dt.TimeOfDay != TimeSpan.Zero)
+                {
+                    return HttpUtility.UrlEncode(((DateTime)val).ToString("o"));
+                }
+
+                return dt.ToString("yyyy-MM-dd");
+
+            }
+            return HttpUtility.UrlEncode(val.ToString());
+        }
+    }
+}
