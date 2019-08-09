315344e6287c51ab612f23a48a66cd185952007a
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2019-08-09 10:45:33 +0200

    Test Modal dialog

---
+++ Src/Server/ClientApp/src/app/modal/modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { DialogData } from '../DialogData';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<ModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit() {
+  }
+
+}
